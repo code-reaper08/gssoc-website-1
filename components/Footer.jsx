e2232f15e89844eadac21e010b83f444396abdd3
MEHAK AGGARWAL <mehakagg1211@gmail.com>
2022-01-22 12:53:40 +0530

    changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -56,23 +56,7 @@ const Footer = () =>{
         </div>
       </div>
   </footer>
-  /* 
-		<footer className="bg-orange-500">
-      <ul className="flex flex-grow items-center justify-center lg:container px-4 py-6 mx-auto text-sm text-white md:px-6">
-        <li>
-          Created by{" "}
-          <a
-            href="https://taylorbryant.dev/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="font-bold"
-          >
-            GSSoC
-          </a>
-        </li>
 
-      </ul>
-    </footer>*/
 	)
 }
 export default Footer;
