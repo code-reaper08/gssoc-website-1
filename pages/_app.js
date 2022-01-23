266b143a1389bbe15411f0abc3861713a4b81fbe
Vishwa.R <v.i.s.h.w.a.08.11.2001@gmail.com>
2022-01-23 15:22:16 +0530

    Fix the semicolon render
    - delete the ; on Line7 of _app.js
    - fixes the above  unnecessary semicolon to render

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,9 @@ import Layout from "../components/Layout";
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </Layout>
-  
-  )
+  );
 }
 
 export default MyApp;
