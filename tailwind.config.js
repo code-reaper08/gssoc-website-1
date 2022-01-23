1b8e6f30e308d0d68182ada7612456ae8cf5fab0
MEHAK AGGARWAL <mehakagg1211@gmail.com>
2022-01-23 13:42:17 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
+  
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  experimental: {
+    darkModeVariant: true
+  },
+  dark: 'class',
   theme: {
     fontFamily: {
       serif: ["Rubik", "ui-sans-serif"],
