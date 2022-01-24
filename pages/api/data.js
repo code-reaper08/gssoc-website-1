32b00c6f4342f2752d821c84f7fb295988087356
anubhavdevv <201241@juitsolan.in>
2022-01-24 22:11:25 +0530

    Modified Faqs section and TeamSection improve the resposiveness of the website

---
+++ pages/api/data.js
@@ -0,0 +1,27 @@
+export const accordionData = [
+    {
+        id: 1,
+        question: 'Why GSSOC? What Do You Mean By Open source',
+        answer: 'Open Source is contributing to the open source software '
+    },
+    {
+        id: 2,
+        question: 'Why GSSOC? What Do You Mean By Open source',
+        answer: 'Open Source is contributing to the open source software '
+    },
+    {
+        id: 3,
+        question: 'Why GSSOC? What Do You Mean By Open source',
+        answer: 'Open Source is contributing to the open source software '
+    },
+    {
+        id: 4,
+        question: 'Why GSSOC? What Do You Mean By Open source',
+        answer: 'Open Source is contributing to the open source software '
+    },
+    {
+        id: 5,
+        question: 'Why GSSOC? What Do You Mean By Open source',
+        answer: 'Open Source is contributing to the open source software '
+    }
+];
